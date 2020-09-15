22d0d54342507f2fded3781f57a367de25f7ff21
Andrew Alteri <andrewsalteri@gmail.com>
2020-09-15 10:46:40 -0500

    Created outline for CharactersController and set 2 private methods(set_character, and character_params).

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,43 @@
 class CharactersController < ApplicationController
+    before_action :set_character, only: [:show, :edit, :update, :destroy] 
+    
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def edit
+
+    end
+
+    def create 
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def set_character
+        @character = Character.find(params[:id])
+    end
+
+    def character_params
+        params.require(:character).permit(:name)
+    end
+
+
 end
