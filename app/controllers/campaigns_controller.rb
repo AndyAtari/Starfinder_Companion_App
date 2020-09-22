69d532283be9cdb9f81dc9a9c339046105c779e1
Andrew Alteri <andrewsalteri@gmail.com>
2020-09-22 13:09:42 -0500

    Built Campaign Controller layout and added index view page.

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,48 @@
 class CampaignsController < ApplicationController
+    before_action :set_campaign, only: [:show, :edit, :update, :destroy] 
+    
+    def index
+        @campaign = Campaign.all
+    end
+
+    def show
+
+    end
+
+    def new
+        @campaign = Campaign.new
+    end
+
+    def edit
+
+    end
+
+    def create 
+        @campaign = Campaign.new(campaign_params)
+        if @campaign.save 
+            redirect_to campaign_path(@campaign)
+        else  
+            render "new"
+        end
+    end
+
+    def update
+        @campaign.update(campaign_params)
+        redirect_to campaign_path(@campaign)
+    end
+
+    def destroy
+        @campaign.destroy
+        redirect_to campaigns_url 
+    end
+
+    private
+
+    def set_campaign
+        @campaign = Campaign.find(params[:id])
+    end
+
+    def campaign_params
+        params.require(:campagin).permit(:title, :adventure_path, :game_master, :looking_for_players, :character_id)
+    end
 end
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,12 @@
 class Campaign < ApplicationRecord
     has_many :characters
+
+
+    def need_players?
+        if self.looking_for_players == true
+            "YES!"
+        else  
+            "No"
+        end
+    end
 end
