0f875655d4f261093d7008e2653bbf4d592eb86f
Andrew Alteri <andrewsalteri@gmail.com>
2020-09-26 15:41:25 -0500

    Added user validations and new user error_explanation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
     has_many :characters
     has_many :campaigns, through: :characters
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+
+    validates :user_name, :email, presence: true
+    validates :user_name, :email, uniqueness: true
+    validates :user_name, length: { minimum: 3, maximum: 25 }
+    validates :email, length: { minimum: 10, maximum: 200 }, format: { with: VALID_EMAIL_REGEX }
 end
