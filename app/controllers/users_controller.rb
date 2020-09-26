1e948d3ed16f1bbd4fb962133e7a0c8eb21c5a64
Andrew Alteri <andrewsalteri@gmail.com>
2020-09-26 15:12:40 -0500

    Built users controller to be able to instantiate a new user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new 
+    end
+
+    def create
+        @user = User.new(user_params)
+        @user.save
+        redirect_to root_path
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :user_name,
+            :email,
+            :password
+        )
+    end
+
 end
