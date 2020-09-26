5fe0c31db6cc11336146e60621eb0851d7ad7101
Andrew Alteri <andrewsalteri@gmail.com>
2020-09-26 16:15:26 -0500

    Added border styling to validation errors.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
     validates :user_name, :email, presence: true
     validates :user_name, :email, uniqueness: true
     validates :user_name, length: { minimum: 3, maximum: 25 }
-    validates :email, length: { minimum: 10, maximum: 200 }, format: { with: VALID_EMAIL_REGEX }
+    validates :email, length: { minimum: 10, maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 end
