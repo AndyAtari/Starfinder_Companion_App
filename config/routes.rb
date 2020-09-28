fe55cf772de35aab3a70a986e28fb13937d97c69
Andrew Alteri <andrewsalteri@gmail.com>
2020-09-28 12:07:07 -0500

    Changed sign out from button to link. WIP!

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
-  post '/signout' => 'sessions#destroy'
+  get '/signout' => 'sessions#destroy'
   
   
   resources :users
