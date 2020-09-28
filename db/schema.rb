dd551a873b4a8bc31b0950e1bdbd385dd3224ff0
Andrew Alteri <andrewsalteri@gmail.com>
2020-09-27 23:21:50 -0500

    Removed unused user_id column in campaigns.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_155041) do
+ActiveRecord::Schema.define(version: 2020_09_28_041802) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string "title"
