7f2e8a329c9391a29c5e1def192e3edb274b0ed0
Andrew Alteri <andrewsalteri@gmail.com>
2020-09-29 15:44:38 -0500

    Refactored user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
     validates :user_name, :email, uniqueness: true
     validates :user_name, length: { minimum: 3, maximum: 25 }
     validates :email, length: { minimum: 10, maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-    #validates :password, presence: true, length: { minimum: 6 }
+    
 
     def self.from_omniauth(auth)
         find_or_create_by(user_name: auth[:info][:nickname]) do |user|
