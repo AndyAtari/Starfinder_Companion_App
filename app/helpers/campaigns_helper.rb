9d11346ffc8ea296c61eac2ad4aa2b4567d00e05
Andrew Alteri <andrewsalteri@gmail.com>
2020-10-04 15:29:45 -0500

    Added campaign helper methods to refactor campaign_table.

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -3,4 +3,16 @@ module CampaignsHelper
     def campaign_order
         @campaigns.group(:title)
     end
+
+    def owned?(campaign)
+        campaign.user_id == current_user.id
+    end
+
+    def starship_count(campaign)
+        campaign.starships.count
+    end
+
+    def starships?(campaign)
+        !campaign.starships.empty?
+    end
 end
